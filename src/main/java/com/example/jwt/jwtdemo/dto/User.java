fe98ffc3361a86b251751d181aebc548b0767b74
junior <junior670@hotmail.com>
2020-10-23 18:48:10 -0500

    primer commit

---
+++ src/main/java/com/example/jwt/jwtdemo/dto/User.java
@@ -0,0 +1,24 @@
+package com.example.jwt.jwtdemo.dto;
+
+public class User {
+
+	private String user;
+	private String token;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
